d2e604c97f3ae5761edca2221273876271cda816
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 11:51:59 +0800

    增加 inputText 元件與雙向綁定

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
-    <h1>HelloWorld to PrimeTime</h1>
+    <span class="p-float-label">
+        <InputText id="txt" type="text" v-model="text" />
+        <label for="txt">Text</label>
+    </span>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from "vue";
+const text = ref();
 </script>
 
 <style scoped>
