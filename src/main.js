182fcf42ca0612dc1d3b848e22dc1e1405969fde
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 10:15:26 +0800

    加入 primeVue 主題核心與圖示資源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 
+import "primevue/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core
+import "primeicons/primeicons.css"; //icons
+
 const app = createApp(App);
 app.use(PrimeVue);
 app.mount('#app');
