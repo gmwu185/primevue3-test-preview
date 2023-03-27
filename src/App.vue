e8dbb54dc919453a604ea35253ed7e82bf0a8152
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 10:08:07 +0800

    加入預設樣式與基本 Vue 結構

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>HelloWorld to PrimeTime</h1>
+  <div class="container">
+    <h1>HelloWorld to PrimeTime</h1>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
 </style>
