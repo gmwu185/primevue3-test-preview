d3b125cf1c52bc3dba4d6a09ed4a6214de9237c0
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 10:22:46 +0800

    1.加入 InputText Button Toast 全域元件  2.引用 Toast 核心 (ToastService)

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 
-import "primevue/resources/themes/lara-light-indigo/theme.css"; //theme
-import "primevue/resources/primevue.min.css"; //core
-import "primeicons/primeicons.css"; //icons
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core
+import 'primeicons/primeicons.css'; //icons
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.use(ToastService);
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
 app.mount('#app');
