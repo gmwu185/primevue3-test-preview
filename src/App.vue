be28448f805e8b22f880bcea127b0e9612f1377d
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 17:53:41 +0800

    增加 primeflex (primeVeu 專用基底樣式) 與 primeVeu 客製主題

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,20 @@
     <div class="container__inter">
       container__inter
     </div>
+    <div class="abc--bule">abc--bule</div>
+  </div>
+  <div class="card">
+    <div class="card-container blue-container flex align-items-center justify-content-start">
+      <div class="hidden w-4rem h-4rem bg-blue-500 text-white font-bold p-4 border-round mr-3">1</div>
+      <div class="w-4rem h-4rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center p-4 border-round mr-3">2</div>
+      <div class="w-4rem h-4rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center p-4 border-round">3</div>
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-12 md:col-6 lg:col-3 sm:flex-nowrap">A</div>
+    <div class="col-12 md:col-6 lg:col-3">B</div>
+    <div class="col-12 md:col-6 lg:col-3">C</div>
+    <div class="col-12 md:col-6 lg:col-3">D</div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -7,8 +7,19 @@ import Button from 'primevue/button';
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
 
-import 'primevue/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primevue/resources/primevue.min.css'; //core
+// scss & css style
+import 'primeflex/primeflex.scss';
+/** primeflex
+ * Doc: https://www.primefaces.org/primeflex/setup
+ * GitHub: https://github.com/primefaces/primeflex
+ */
+
+// import 'primevue/resources/themes/lara-light-indigo/theme.css'; // 官方預設 theme
+import './assets/scss/theme.scss';
+/** 自定 primevue 主題
+ * primevue-sass-theme: https://github.com/primefaces/primevue-sass-theme
+*/
+import 'primevue/resources/primevue.min.css'; // 官方預設 core
 import 'primeicons/primeicons.css'; //icons
 
 const app = createApp(App);
