7fb5af5244fc4cb6c23befac926f1a9ae6a35f85
jimmy.wu <jimmy.wu@revivalsi.com>
2023-03-27 14:42:51 +0800

    使用 primeVue 元件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
+  <ToastUI />
   <div class="container">
     <span class="p-float-label">
-        <InputText id="txt" type="text" v-model="text" />
-        <label for="txt">Text</label>
+      <InputTextUI id="txt" type="text" v-model="text" />
+      <label for="txt">Text</label>
     </span>
+    <ButtonUI 
+      label="Greet" @click="greet()"
+      icon="pi pi-user"
+    >
+    </ButtonUI>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-const text = ref();
+import { ref } from 'vue';
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+const text = ref('');
+const greet = () => {
+  toast.add({
+    // severity: 'success',
+    summary: 'PrimeTime',
+    detail: text.value,
+  });
+};
 </script>
 
 <style scoped>
-  .container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-  }
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue';
 import PrimeVue from 'primevue/config';
 
 import InputText from 'primevue/inputtext';
-// import Button from 'primevue/button';
-// import Toast from 'primevue/toast';
-// import ToastService from 'primevue/toastservice';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
 import 'primevue/resources/themes/lara-light-indigo/theme.css'; //theme
 import 'primevue/resources/primevue.min.css'; //core
@@ -13,8 +13,8 @@ import 'primeicons/primeicons.css'; //icons
 
 const app = createApp(App);
 app.use(PrimeVue);
-// app.use(ToastService);
-app.component('InputText', InputText);
-// app.component('Button', Button);
-// app.component('Toast', Toast);
+app.use(ToastService);
+app.component('InputTextUI', InputText);
+app.component('ToastUI', Toast);
+app.component('ButtonUI',Button);
 app.mount('#app');
